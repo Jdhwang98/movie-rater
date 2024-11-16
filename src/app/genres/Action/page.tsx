fa23393ddbfb908e3a02d6704f0e0b7b29260e7f
Johnathan <jdhwang98@gmail.com>
2024-11-15 17:30:31 -0800

    fixed the file management for genres to one single file

--- src/app/genres/Action/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-    return (
-        <div>Action PAGE</div>
-    )
-}
-
-export default page
--- src/app/genres/Horror/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-    return (
-        <div>Horror Page</div>
-    )
-}
-
-export default page
