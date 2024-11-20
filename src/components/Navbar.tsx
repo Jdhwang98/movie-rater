d6852ea855c2ccfacf9f9184bc132833a4a1af7f
Harshaun <harshaunkhehra@yahoo.com>
2024-11-19 23:00:43 -0800

    navbar edits

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,22 +2,23 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const Navbar = () => {
-    return (
-        // Logo and Title 
-      <nav className="bg-gray-800 p-4">
-        <div className="max-w-screen-xl mx-auto flex justify-between items-center">
+  return (
+    // Logo and Title 
+    <nav className="bg-gray-800 p-4">
+      <div className="max-w-screen-xl mx-auto flex justify-between items-center">
         <div className="flex items-center text-white text-xl font-bold space-x-2">
           <Link href="/">
             <Image src="/icon.png" alt="Logo" className="h-10 w-13 rounded" height={100} width={300} />
           </Link>
         </div>
 
-          {/* Navigation Links */}
-          <ul className="flex space-x-4">
-            <li><Link href="/" className="text-white hover:text-blue-400">Home</Link></li>
-            <li><Link href="/contact" className="text-white hover:text-blue-400">Contact</Link></li>
-            <li><Link href="/profile" className ="text-white hover:text-blue-400">Profile</Link></li>
-          </ul>  
+        {/* Navigation Links */}
+        <ul className="flex space-x-4">
+          <li><Link href="/" className="text-white hover:text-blue-400">Home</Link></li>
+          <li><Link href="/voting-session" className="text-white hover:text-blue-400">Voting Session</Link></li>
+          <li><Link href="/contact" className="text-white hover:text-blue-400">Contact</Link></li>
+          <li><Link href="/profile" className="text-white hover:text-blue-400">Profile</Link></li>
+        </ul>
       </div>
     </nav>
   );
